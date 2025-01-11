8b464e9c27571e25b9b85af535582786ed8b7b83
choi <146166271+bdbboi@users.noreply.github.com>
2025-01-12 05:49:28 +0900

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,14 +1,23 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for LottoService project.
 
-app_name = 'lotto'
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'),  # 기본 페이지 경로
-    path('buy/', views.buy_lotto, name='buy_lotto'),
-    path('confirmation/<int:ticket_id>/', views.confirmation, name='confirmation'),  # confirmation URL 추가
-    path('draw/', views.draw_winning_numbers, name='draw_winning_numbers'),
-    path('winning-numbers/', views.winning_numbers, name='winning_numbers'),  # 당첨 결과 페이지
-    path('sales/', views.lotto_sales, name='lotto_sales'),  # 로또 판매 통계 페이지
-    path('winning-statistics/', views.winning_statistics, name='winning_statistics'),  # 당첨자 통계 페이지
+    path('admin/', admin.site.urls),
+    path('', include('lotto.urls'))
 ]
